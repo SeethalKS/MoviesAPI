e6867cafcb0c53bf33863c02fdd3137dd23d9008
SeethalKS <seethal007@outlook.com>
2025-04-22 16:27:37 +0530

    Commit from backend

---
+++ MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/genres")]  //[Route("api/[controller]")]
+    public class GenresController
+    {
+        [HttpGet]
+        public List<Genre> Get()
+        {
+            var repository = new InMemoryRepository();
+            var genres = repository.GetAllGenres();
+            return genres;
+        }
+
+        [HttpGet]
+        public Genre? Get(int id)
+        {
+            var repository = new InMemoryRepository();
+            var genre = repository.GetById(id);
+            return genre;
+        }
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+        [HttpPut]
+        public void Put()
+        { 
+        }
+        [HttpDelete]
+        public void Delete() 
+        { 
+        }
+    }
+}
---
+++ MoviesAPI/InMemoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using MoviesAPI.Entities;
+
+namespace MoviesAPI
+{
+    public class InMemoryRepository
+    {
+        private List<Genre> _genres;
+        public InMemoryRepository()
+        {
+            _genres = new List<Genre>
+            {
+                new Genre{Id=1, Name="Comedy"},
+                new Genre{Id=2, Name="Action"}
+            };
+        }
+        public List<Genre> GetAllGenres()
+        { return _genres; }
+
+        public Genre? GetById(int id)
+        {
+            return _genres.FirstOrDefault(g=>g.Id==id);
+        }
+    }
+}
